f46edaedbe827b7364414a995767042fd2b9bb1d
gharbaoui <mohamed9gharbaoui@gmail.com>
2022-10-30 17:41:11 +0100

    classes

--- src/custom_types.ts
+++ src/custom_types.ts
@@ -1,12 +1,19 @@
-interface Point {
+class Point {
     x:number;
     y:number;
+    constructor(x:number, y:number) {
+        this.x = x;
+        this.y = y;
+    }
+    draw() {
+        // console.log(`{ ${this.x}, ${this.y} }`)
+    }
 };
+
 let drawPoint = (point: Point) => {
     console.log(point);
 }
 
-drawPoint({
-    x: 1,
-    y: 2
-});
+const p = new Point(1, 3);
+
+drawPoint(p);
