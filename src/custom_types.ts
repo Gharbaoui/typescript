767901c7b856b1576aa642d95b13cde1e352b70b
gharbaoui <mohamed9gharbaoui@gmail.com>
2022-10-30 17:58:35 +0100

    properties

--- src/custom_types.ts
+++ src/custom_types.ts
@@ -1,10 +1,24 @@
 class Point {
-    constructor(private x:number, private y:number) {
+    constructor(private _x:number, private _y:number) {
     }
     draw() {
-        console.log(`{ ${this.x}, ${this.y} }`)
+        console.log(`{ ${this._x}, ${this._y} }`)
+    }
+
+    get x()
+    {
+        return this._x;
+    }
+
+    set x(val:number)
+    {
+        if (val < 0)
+            throw new Error(`value can not be less than 0`);
+        this._x = val;
     }
 };
 
 const p = new Point(1, 3);
+p.x = 23;
+console.log(p.x);
 p.draw();
