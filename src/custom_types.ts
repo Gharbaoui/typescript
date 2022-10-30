aacd70516bee5da50c6e782b00cd1acb9b77ccdb
gharbaoui <mohamed9gharbaoui@gmail.com>
2022-10-30 18:04:03 +0100

    using point module

--- src/custom_types.ts
+++ src/custom_types.ts
@@ -1,24 +1,3 @@
-class Point {
-    constructor(private _x:number, private _y:number) {
-    }
-    draw() {
-        console.log(`{ ${this._x}, ${this._y} }`)
-    }
-
-    get x()
-    {
-        return this._x;
-    }
-
-    set x(val:number)
-    {
-        if (val < 0)
-            throw new Error(`value can not be less than 0`);
-        this._x = val;
-    }
-};
-
+import { Point } from "./point";
 const p = new Point(1, 3);
-p.x = 23;
-console.log(p.x);
 p.draw();
